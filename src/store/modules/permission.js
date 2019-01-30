7185bbcd7e819ae28553a860e12c7a7e9110ae20
liuyajun <liuyajun@linhuiba.com>
2019-01-30 17:20:59 +0800

    是否显示左侧导航判断方法修改，根据用户可访问路由数组判断

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -34,6 +34,53 @@ function filterAsyncRouter(routes, roles) {
   return res
 }
 
+/**
+ * 通过path判断是否与当前用户可访问路由数组匹配
+ * @param authed_urls
+ * @param route
+ */
+function hasPermissionByAuthedUrls(authed_urls, route) {
+  if (route.path === '' || route.path === '/') {
+    return true
+  } else {
+    return authed_urls.some(authed_url => {
+      return authed_url.split('|').some(url => {
+        if (url.indexOf('/{id}') >= 0) {
+          // 使 如 /resources/{id} 能够匹配到 /resources/1
+          var re = new RegExp(url.replace('/{id}', '/(\\d)*'))
+          if (re.test(route.path)) {
+            return true
+          }
+        } else if (url === route.path) {
+          return true
+        }
+        return false
+      })
+    })
+  }
+}
+
+/**
+ * 递归过滤异步路由表，返回符合用户可访问路由数组的路由表
+ * @param routes asyncRouterMap
+ * @param authed_urls
+ */
+function filterAsyncRouterByAuthedUrls(routes, authed_urls) {
+  const res = []
+
+  routes.forEach(route => {
+    const tmp = { ...route }
+    if (hasPermissionByAuthedUrls(authed_urls, tmp)) {
+      if (tmp.children) {
+        tmp.children = filterAsyncRouterByAuthedUrls(tmp.children, authed_urls)
+      }
+      res.push(tmp)
+    }
+  })
+
+  return res
+}
+
 const permission = {
   state: {
     routers: constantRouterMap,
@@ -65,12 +112,19 @@ const permission = {
   actions: {
     GenerateRoutes({ commit }, data) {
       return new Promise(resolve => {
-        const { roles } = data
+        const { roles, authed_urls } = data
+        // asyncRouterMap 动态路由，根据权限判断是否插入到路由数组中
+        // roles          用户角色，如果动态路由中包含 meta.roles: ['admin', 'editor']，及当角色为admin、editor才能看到次路由
+        // authed_urls    可访问路由，当前登录用户能够（看到并）访问的路由
         let accessedRouters
         if (roles.includes('admin')) {
           accessedRouters = asyncRouterMap
         } else {
-          accessedRouters = filterAsyncRouter(asyncRouterMap, roles)
+          // accessedRouters = filterAsyncRouter(asyncRouterMap, roles)
+          console.log(asyncRouterMap, roles, authed_urls)
+          // accessedRouters = filterAsyncRouterByAuthedUrls(asyncRouterMap, authed_urls)
+          accessedRouters = filterAsyncRouterByAuthedUrls(asyncRouterMap, ['/users', '/users/view/{id}', '/businesses/edit|/businesses/edit/{id}'])
+          // accessedRouters = asyncRouterMap
         }
         commit('SET_ROUTERS', accessedRouters)
         resolve()
