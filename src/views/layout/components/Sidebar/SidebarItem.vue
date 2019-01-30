6d466ffeb3d22f2fd612e38967f3e3573d53c1c4
liuyajun <liuyajun@linhuiba.com>
2019-01-30 17:20:24 +0800

    导航可根据onlyHideParent是否显示当前导航（隐藏后，以各子路由为根路由，展开显示）

--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -1,32 +1,53 @@
 <template>
   <div v-if="!item.hidden&&item.children" class="menu-wrapper">
-
-    <template v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
+    <!-- 如果是该路由只有一个子路由，且alwaysShow为false，那么以子路由为根路由 -->
+    <!-- <template v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
       <app-link :to="resolvePath(onlyOneChild.path)">
         <el-menu-item :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
           <item v-if="onlyOneChild.meta" :icon="onlyOneChild.meta.icon||item.meta.icon" :title="onlyOneChild.meta.title" />
         </el-menu-item>
       </app-link>
+    </template> -->
+    <!-- 如果（该路由只有一个子路由，且alwaysShow为false）或（onlyHideParent为true），不显示父路由，以各子路由为根路由，展开显示 -->
+    <template v-if="item.onlyHideParent || (hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow)">
+      <template v-for="child in item.children">
+        <template v-if="!child.hidden">
+          <sidebar-item
+            v-if="child.children&&child.children.length>0"
+            :is-nest="true"
+            :item="child"
+            :key="child.path"
+            :base-path="resolvePath(child.path)"
+            class="nest-menu" />
+          <app-link v-else :to="resolvePath(child.path)" :key="child.name">
+            <el-menu-item :index="resolvePath(child.path)">
+              <item v-if="child.meta" :path="child.path" :icon="child.meta.icon" :title="child.meta.title" />
+            </el-menu-item>
+          </app-link>
+        </template>
+      </template>
     </template>
-
+    <!-- 否则，正常创建路由 -->
     <el-submenu v-else :index="resolvePath(item.path)">
       <template slot="title">
-        <item v-if="item.meta" :icon="item.meta.icon" :title="item.meta.title" />
+        <item v-if="item.meta" :path="item.path" :icon="item.meta.icon" :title="item.meta.title" />
       </template>
 
-      <template v-for="child in item.children" v-if="!child.hidden">
-        <sidebar-item
-          v-if="child.children&&child.children.length>0"
-          :is-nest="true"
-          :item="child"
-          :key="child.path"
-          :base-path="resolvePath(child.path)"
-          class="nest-menu" />
-        <app-link v-else :to="resolvePath(child.path)" :key="child.name">
-          <el-menu-item :index="resolvePath(child.path)">
-            <item v-if="child.meta" :icon="child.meta.icon" :title="child.meta.title" />
-          </el-menu-item>
-        </app-link>
+      <template v-for="child in item.children">
+        <template v-if="!child.hidden">
+          <sidebar-item
+            v-if="child.children&&child.children.length>0"
+            :is-nest="true"
+            :item="child"
+            :key="child.path"
+            :base-path="resolvePath(child.path)"
+            class="nest-menu" />
+          <app-link v-else :to="resolvePath(child.path)" :key="child.name">
+            <el-menu-item :index="resolvePath(child.path)">
+              <item v-if="child.meta" :path="child.path" :icon="child.meta.icon" :title="child.meta.title" />
+            </el-menu-item>
+          </app-link>
+        </template>
       </template>
     </el-submenu>
 
