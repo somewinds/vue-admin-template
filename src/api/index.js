42322a7dd6db8f11187b631e38120516b0592337
liuyajun <liuyajun@linhuiba.com>
2019-01-30 11:50:37 +0800

    增加获取权限接口

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,18 @@
 // import { showValidator, post, get, put, remove, jsonToUrlQuery } from '@/utils/request'
-import { post } from '@/utils/request'
+import { get, post } from '@/utils/request'
 
 // api调用封装
 // 用户登录
 export function postLogin(param, successCallback, errorCallback) {
   post('/inner/login', param, successCallback, errorCallback, errorCallback)
 }
+
 // 退出登录
 export function postLogout(param, successCallback, errorCallback) {
   post('/inner/logout', param, successCallback, errorCallback, errorCallback)
+}
+
+// 获取权限
+export function getPermission(param, successCallback, errorCallback) {
+  get('/inner/permissions/view', param, successCallback, errorCallback, errorCallback)
 }
